09b83e265394534b4307546fa71a70bd904b686d
deepan1920 <dp7708@srmist.edu.in>
2026-03-25 09:49:44 +0530

    UC3:Centralized Room Inventory Management

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,6 +2,7 @@ import java.util.*;
 import java.lang.String;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
+        //UC1
         System.out.println("Welcome to Palindrome Checker Management System");
         System.out.println("Version ; 1.0");
         System.out.println("System initialized successfully!");
@@ -9,13 +10,24 @@ public class PalindromeCheckerApp {
         String str = new String("madam");
         String isPalindrome = new String("True");
         int j = str.length();
-        for (int i = 0; i < str.length() / 2; i++) {
-            j = j - 1;
-            if (str.charAt(i) != str.charAt(j)) {
-                isPalindrome = "False";
-            }
+        // UC2
+//        for (int i = 0; i < str.length() / 2; i++) {
+//            j = j - 1;
+//            if (str.charAt(i) != str.charAt(j)) {
+//                isPalindrome = "False";
+//            }
+//        }
+//        if (isPalindrome.equals("True")) {
+//            System.out.println("Is it a palindrome?: True");
+//        }else{
+//            System.out.println("Is it a palindrome?: False");
+//        }
+        String rev=new String("");
+        for(int i=str.length()-1;i>=0;i--){
+            rev+=str.charAt(i);
         }
-        if (isPalindrome.equals("True")) {
+
+        if(str.equals(rev)){
             System.out.println("Is it a palindrome?: True");
         }else{
             System.out.println("Is it a palindrome?: False");
